09d719aa787491d9284f1bbc5f6ed13d73e0db32
Tom Stuart <tom@codon.com>
2017-02-19 10:23:40 +0000

    Extract parser spec

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -26,35 +26,9 @@ RSpec.describe do
       Builder.new.build_symbol(name)
     end
 
-    def yes
-      symbol('true')
-    end
-
-    def no
-      symbol('false')
-    end
-
-    def conditional(condition, consequent, alternative)
-      Builder.new.build_formula([
-        symbol('if'),   condition,
-        symbol('then'), consequent,
-        symbol('else'), alternative
-      ])
-    end
-
     def evaluates(before, after)
       Builder.new.build_formula([before, symbol('→'), after])
     end
-
-    expect(parse('true')).to eq yes
-    expect(parse('if false then false else true')).to eq conditional(no, no, yes)
-    expect(parse('if (if true then true else false) then false else true')).to eq conditional(conditional(yes, yes, no), no, yes)
-
-    expect(parse('(if true then _t₂ else _t₃) → _t₂')).to look_like 'if true then t₂ else t₃ → t₂'
-    expect(parse('(if false then _t₂ else _t₃) → _t₃')).to look_like 'if false then t₂ else t₃ → t₃'
-
-    expect(parse('_t₁ → _t₁′')).to look_like 't₁ → t₁′'
-    expect(parse('(if _t₁ then _t₂ else _t₃) → (if _t₁′ then _t₂ else _t₃)')).to look_like 'if t₁ then t₂ else t₃ → if t₁′ then t₂ else t₃'
 
     def find_variable(formula, name)
       case formula
