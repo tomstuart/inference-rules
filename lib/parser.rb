38e22010d6759b01836e541ed385fa3616c701d7
Tom Stuart <tom@codon.com>
2017-02-19 09:47:43 +0000

    Make parser generic
    Now that we have annotations for nested structure and variables, we can
    just turn everything else into symbols — we don’t need to know the
    specific structures that the symbols represent.
    
    In turn, this means that we don’t need to distinguish between parsing a
    term and formula. These processes are identical now.

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,7 +5,7 @@ class Parser
 
   def parse_complete_formula(string)
     self.string = string
-    parse_everything { parse_formula }
+    parse_everything { parse_term }
   end
 
   def parse_complete_term(string)
@@ -27,36 +27,24 @@ class Parser
     result
   end
 
-  def parse_formula
-    left = parse_term
-    symbol = read %r{→|∈}
-    right = parse_term
+  def parse_term
+    atoms = []
+    atoms << parse_atom until can_read? %r{\)|\z}
 
-    case symbol
-    when '→'
-      builder.build_evaluates(left, right)
-    when '∈'
-      builder.build_element_of(left, right)
+    if atoms.length == 1
+      atoms.first
     else
-      complain
+      Formula.new(atoms)
     end
   end
 
-  def parse_term
+  def parse_atom
     if can_read? %r{\(}
       parse_brackets
-    elsif can_read? %r{if}
-      parse_conditional
-    elsif can_read? %r{true|false}
-      parse_boolean
-    elsif can_read? %r{0}
-      parse_zero
-    elsif can_read? %r{succ|pred|iszero}
-      parse_numeric_operation
-    elsif can_read? %r{[[:upper:]]+}
-      parse_constant
-    elsif can_read? %r{_[^\p{Blank}()]+}
+    elsif can_read? %r{_}
       parse_variable
+    elsif can_read? %r{[^\p{Blank}()]+}
+      parse_symbol
     else
       complain
     end
@@ -70,71 +58,20 @@ class Parser
     term
   end
 
-  def parse_conditional
-    read %r{if}
-    condition = parse_term
-    read %r{then}
-    consequent = parse_term
-    read %r{else}
-    alternative = parse_term
-
-    builder.build_conditional(condition, consequent, alternative)
-  end
-
-  def parse_boolean
-    case read_boolean
-    when 'true'
-      builder.build_true
-    when 'false'
-      builder.build_false
-    else
-      complain
-    end
-  end
-
-  def parse_zero
-    read %r{0}
-    builder.build_zero
-  end
-
-  def parse_numeric_operation
-    numeric_operation = read_numeric_operation
-    argument = parse_term
-
-    case numeric_operation
-    when 'succ'
-      builder.build_succ(argument)
-    when 'pred'
-      builder.build_pred(argument)
-    when 'iszero'
-      builder.build_iszero(argument)
-    else
-      complain
-    end
-  end
-
-  def parse_constant
-    builder.build_constant(read_constant)
-  end
-
   def parse_variable
     builder.build_variable(read_name)
   end
 
-  def read_boolean
-    read %r{true|false}
-  end
-
-  def read_numeric_operation
-    read %r{succ|pred|iszero}
-  end
-
-  def read_constant
-    read %r{[[:upper:]]+}
+  def parse_symbol
+    builder.build_symbol(read_symbol)
   end
 
   def read_name
     read %r{_}
+    read_symbol
+  end
+
+  def read_symbol
     read %r{[^\p{Blank}()]+}
   end
 
