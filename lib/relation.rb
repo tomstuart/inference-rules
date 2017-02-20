17c7ab0245e396e394f80d3e222d06fc352ae3f0
Tom Stuart <tom@codon.com>
2017-02-20 19:16:21 +0000

    Add Relation.define factory method

--- lib/relation.rb
+++ lib/relation.rb
@@ -1,8 +1,14 @@
 require 'ast/builder'
+require 'definition'
+require 'rule'
 
 class Relation
   def initialize(name, definition)
     self.name, self.definition = name, definition
+  end
+
+  def self.define(name:, rules:)
+    new(name, Definition.new(rules.map(&Rule.method(:define))))
   end
 
   NoRuleApplies = Class.new(StandardError)
