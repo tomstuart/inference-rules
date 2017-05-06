4ca13a46bd0a70559ba4afcecdb7148bf4b091c3
Tom Stuart <tom@codon.com>
2017-05-06 21:26:45 +0100

    Implement typechecking for simply typed lambda calculus expressions
    Slightly ridiculously, this doesn’t require any new machinery apart from
    the n-ary relation support in 9b7eebc2a8b697b012b2a132cd45a78d69c4edf6.
    
    The important rules are just typed in from page 103 of TAPL. The
    supporting rules are more elaborate than usual because we need to
    support variable names and typing contexts.
    
    The syntax rules support variables by enumerating a few strings
    (“a”…“g”, “x”, “y”, “z”) that are syntactically valid variables. This is
    pretty simple-minded but sufficient for writing moderately complex terms
    without running out of names.
    
    Typing contexts are more complicated because they can grow to an
    arbitrary size. To accommodate this we encode them in a nested list
    structure:
    
    * “∅” is the empty context; then
    * “(a : Bool , ∅)” is the empty context extended with the assumption
      that the variable a has type Bool; then
    * “(b : (Bool → Bool) , (a : Bool , ∅))” is the context which also
      assumes that the variable b has type Bool → Bool; and so on.
    
    Fortunately we can define another relation, “_Г assumes (_x : _T)”,
    which lets us look up variables recursively in this structure.
    
    (I suspect this won’t work right when one bound variable shadows
    another. I can’t see how to prevent the recursive search continuing
    beyond the first occurrence of the desired variable if the type doesn’t
    match, which probably allows untypable terms to be given a type. For now
    I’ll avoid the problem by assuming the Barendregt convention, i.e. all
    variable names are distinct.)
    
    The weird whitespace around lambdas, colons, full stops and commas is
    necessary to separate them from their surroundings because the parser
    splits tokens on whitespace — brackets get special treatment, but no
    other punctuation does. I could fix this by coming up with more
    elaborate rules for how tokens are separated, but I apparently haven’t.

--- src/typechecking.spec.js
+++ src/typechecking.spec.js
@@ -140,7 +140,7 @@ describe('typechecking', () => {
     });
 
     expect.extend({
-      toTypecheck(term, type) {
+      toArithmeticTypecheck(term, type) {
         const actualTerm = parse(term);
         const expectedType = type && parse(type);
         let actualType;
@@ -168,31 +168,234 @@ describe('typechecking', () => {
     });
 
     test('', () => {
-      expect('pred (succ (succ 0))').toTypecheck('Nat');
+      expect('pred (succ (succ 0))').toArithmeticTypecheck('Nat');
     });
 
     test('', () => {
       expect(
         'if (iszero (succ 0)) then (succ (pred 0)) else (pred (succ 0))'
-      ).toTypecheck('Nat');
+      ).toArithmeticTypecheck('Nat');
     });
 
     test('', () => {
       expect(
         'if (iszero (pred 0)) then (iszero 0) else (iszero (succ 0))'
-      ).toTypecheck('Bool');
+      ).toArithmeticTypecheck('Bool');
     });
 
     test('', () => {
-      expect('if (succ 0) then 0 else (succ 0)').not.toTypecheck();
+      expect('if (succ 0) then 0 else (succ 0)').not.toArithmeticTypecheck();
     });
 
     test('', () => {
-      expect('if (iszero 0) then 0 else (iszero 0)').not.toTypecheck();
+      expect(
+        'if (iszero 0) then 0 else (iszero 0)'
+      ).not.toArithmeticTypecheck();
     });
 
     test('', () => {
-      expect('if (iszero true) then 0 else (succ 0)').not.toTypecheck();
+      expect(
+        'if (iszero true) then 0 else (succ 0)'
+      ).not.toArithmeticTypecheck();
+    });
+  });
+
+  const lambdaCalculusTermSyntax = [
+    { conclusion: 'a ∈ x' },
+    { conclusion: 'b ∈ x' },
+    { conclusion: 'c ∈ x' },
+    { conclusion: 'd ∈ x' },
+    { conclusion: 'e ∈ x' },
+    { conclusion: 'f ∈ x' },
+    { conclusion: 'g ∈ x' },
+    { conclusion: 'x ∈ x' },
+    { conclusion: 'y ∈ x' },
+    { conclusion: 'z ∈ x' },
+    {
+      premises: List.of('_x ∈ x'),
+      conclusion: '_x ∈ t'
+    },
+    {
+      premises: List.of('_x ∈ x', '_T ∈ T', '_t ∈ t'),
+      conclusion: '(λ _x : _T . _t) ∈ t'
+    },
+    {
+      premises: List.of('_t₁ ∈ t', '_t₂ ∈ t'),
+      conclusion: '(_t₁ _t₂) ∈ t'
+    }
+  ];
+
+  const lambdaCalculusTypeSyntax = [
+    {
+      premises: List.of('_T₁ ∈ T', '_T₂ ∈ T'),
+      conclusion: '(_T₁ → _T₂) ∈ T'
+    }
+  ];
+
+  const lambdaCalculusTypingContextRules = [
+    { conclusion: '∅ ∈ Г' },
+    {
+      premises: List.of('_x ∈ x', '_T ∈ T', '_Г ∈ Г'),
+      conclusion: '(_x : _T , _Г) ∈ Г'
+    },
+    {
+      premises: List.of('_x ∈ x', '_T ∈ T', '_Г ∈ Г'),
+      conclusion: '(_x : _T , _Г) assumes (_x : _T)'
+    },
+    {
+      premises: List.of(
+        '_Г assumes (_x₂ : _T₂)',
+        '_x₁ ∈ x',
+        '_x₂ ∈ x',
+        '_T₁ ∈ T',
+        '_T₂ ∈ T',
+        '_Г ∈ Г'
+      ),
+      conclusion: '(_x₁ : _T₁ , _Г) assumes (_x₂ : _T₂)'
+    }
+  ];
+
+  const lambdaCalculusTypeRules = [
+    {
+      premises: List.of('_Г ∈ Г'),
+      conclusion: '_Г ⊢ true : Bool'
+    },
+    {
+      premises: List.of('_Г ∈ Г'),
+      conclusion: '_Г ⊢ false : Bool'
+    },
+    {
+      premises: List.of(
+        '_Г ⊢ _t₁ : Bool',
+        '_Г ⊢ _t₂ : _T',
+        '_Г ⊢ _t₃ : _T',
+        '_Г ∈ Г',
+        '_t₁ ∈ t',
+        '_t₂ ∈ t',
+        '_t₃ ∈ t',
+        '_T ∈ T'
+      ),
+      conclusion: '_Г ⊢ (if _t₁ then _t₂ else _t₃) : _T'
+    },
+    {
+      premises: List.of('_Г assumes (_x : _T)', '_x ∈ x', '_T ∈ T', '_Г ∈ Г'),
+      conclusion: '_Г ⊢ _x : _T'
+    },
+    {
+      premises: List.of(
+        '(_x : _T₁ , _Г) ⊢ _t₂ : _T₂',
+        '_Г ∈ Г',
+        '_x ∈ x',
+        '_T₁ ∈ T',
+        '_t₂ ∈ t',
+        '_T₂ ∈ T'
+      ),
+      conclusion: '_Г ⊢ (λ _x : _T₁ . _t₂) : (_T₁ → _T₂)'
+    },
+    {
+      premises: List.of(
+        '_Г ⊢ _t₁ : (_T₁₁ → _T₁₂)',
+        '_Г ⊢ _t₂ : _T₁₁',
+        '_Г ∈ Г',
+        '_t₁ ∈ t',
+        '_T₁₁ ∈ T',
+        '_T₁₂ ∈ T',
+        '_t₂ ∈ t'
+      ),
+      conclusion: '_Г ⊢ (_t₁ _t₂) : _T₁₂'
+    }
+  ];
+
+  describe('lambda calculus expressions', () => {
+    const lambdaCalculusTypechecking = Relation.define({
+      name: ['⊢', ':'],
+      rules: List.of(
+        ...booleanTermSyntax,
+        ...lambdaCalculusTermSyntax,
+        ...booleanTypeSyntax,
+        ...lambdaCalculusTypeSyntax,
+        ...lambdaCalculusTypingContextRules,
+        ...lambdaCalculusTypeRules
+      )
+    });
+
+    expect.extend({
+      toTypecheck(term, type, context) {
+        const actualContext = parse(context || '∅');
+        const actualTerm = parse(term);
+        const expectedType = type && parse(type);
+        let actualType;
+
+        try {
+          actualType = lambdaCalculusTypechecking.once(
+            actualContext,
+            actualTerm
+          );
+
+          if (!expectedType || actualType.equals(expectedType)) {
+            return {
+              pass: true,
+              message: `expected ${term} not to typecheck as “${type}”`
+            };
+          }
+        } catch (e) {
+          if (!e instanceof NoRuleApplies) {
+            throw e;
+          }
+        }
+
+        return {
+          pass: false,
+          message: `expected ${term} to typecheck as “${type}”, but got “${actualType}”`
+        };
+      }
+    });
+
+    describe('', () => {
+      test('', () => {
+        expect('true').toTypecheck('Bool');
+      });
+      test('', () => {
+        expect('a').toTypecheck('Bool', '(a : Bool , ∅)');
+      });
+      test('', () => {
+        expect('λ a : Bool . a').toTypecheck('Bool → Bool');
+      });
+      test('', () => {
+        expect('a true').toTypecheck('Bool', '(a : (Bool → Bool) , ∅)');
+      });
+
+      test('', () => {
+        expect('(λ a : Bool . a) true').toTypecheck('Bool');
+      });
+      test('', () => {
+        expect('(λ a : (Bool → Bool) . a) (λ b : Bool . b)').toTypecheck(
+          'Bool → Bool'
+        );
+      });
+      test('', () => {
+        expect('(λ a : Bool . a) (λ b : Bool . b)').not.toTypecheck();
+      });
+
+      test('', () => {
+        expect('f (if false then true else false)').not.toTypecheck();
+      });
+      test('', () => {
+        expect('f (if false then true else false)').toTypecheck(
+          'Bool',
+          '(f : (Bool → Bool) , ∅)'
+        );
+      });
+
+      test('', () => {
+        expect('λ x : Bool . (f (if x then false else x))').not.toTypecheck();
+      });
+      test('', () => {
+        expect('λ x : Bool . (f (if x then false else x))').toTypecheck(
+          'Bool → Bool',
+          '(f : (Bool → Bool) , ∅)'
+        );
+      });
     });
   });
 });
